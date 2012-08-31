79b0c45108c3c1ce1ff8f8777d07b5971edeaad5
Matthew Weier O'Phinney <matthew@zend.com>
2012-08-31 14:44:59 -0500

    [zendframework/zf2#2284][ZF2-507] Updated README
    - Notice about Date header

---
+++ test/ElementTest.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @package   Zend_Form
+ */
+
+namespace ZendTest\Form;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Form\Element;
+use Zend\Form\ElementInterface;
+
+/**
+ * @category   Zend
+ * @package    Zend_Form
+ * @subpackage UnitTest
+ */
+class ElementTest extends TestCase
+{
+    public function testAttributesAreEmptyByDefault()
+    {
+        $element = new Element();
+        $this->assertEquals(array(), $element->getAttributes());
+    }
+
+    public function testCanAddAttributesSingly()
+    {
+        $element = new Element();
+        $element->setAttribute('data-foo', 'bar');
+        $this->assertEquals('bar', $element->getAttribute('data-foo'));
+    }
+
+    public function testCanAddManyAttributesAtOnce()
+    {
+        $element = new Element();
+        $attributes = array(
+            'type'     => 'text',
+            'class'    => 'text-element',
+            'data-foo' => 'bar',
+        );
+        $element->setAttributes($attributes);
+        $this->assertEquals($attributes, $element->getAttributes());
+    }
+
+    public function testAddingAttributesMerges()
+    {
+        $element = new Element();
+        $attributes = array(
+            'type'     => 'text',
+            'class'    => 'text-element',
+            'data-foo' => 'bar',
+        );
+        $attributesExtra = array(
+            'data-foo' => 'baz',
+            'width'    => 20,
+        );
+        $element->setAttributes($attributes);
+        $element->setAttributes($attributesExtra);
+        $expected = array_merge($attributes, $attributesExtra);
+        $this->assertEquals($expected, $element->getAttributes());
+    }
+
+    public function testCanClearAllAttributes()
+    {
+        $element = new Element();
+        $attributes = array(
+            'type'     => 'text',
+            'class'    => 'text-element',
+            'data-foo' => 'bar',
+        );
+        $element->setAttributes($attributes);
+        $element->clearAttributes();
+        $this->assertEquals(array(), $element->getAttributes());
+    }
+
+    public function testSettingNameSetsNameAttribute()
+    {
+        $element = new Element();
+        $element->setName('foo');
+        $this->assertEquals('foo', $element->getAttribute('name'));
+    }
+
+    public function testSettingNameAttributeAllowsRetrievingName()
+    {
+        $element = new Element();
+        $element->setAttribute('name', 'foo');
+        $this->assertEquals('foo', $element->getName());
+    }
+
+    public function testCanPassNameToConstructor()
+    {
+        $element = new Element('foo');
+        $this->assertEquals('foo', $element->getName());
+    }
+
+    public function testCanSetCustomOptionFromConstructor()
+    {
+        $element = new Element('foo', array(
+            'custom' => 'option'
+        ));
+        $options = $element->getOptions();
+        $this->assertArrayHasKey('custom', $options);
+        $this->assertEquals('option', $options['custom']);
+    }
+
+    public function testCanSetCustomOptionFromMethod()
+    {
+        $element = new Element('foo');
+        $element->setOptions(array(
+            'custom' => 'option'
+        ));
+
+        $options = $element->getOptions();
+        $this->assertArrayHasKey('custom', $options);
+        $this->assertEquals('option', $options['custom']);
+    }
+
+    public function testCanRetrieveSpecificOption()
+    {
+        $element = new Element('foo');
+        $element->setOptions(array(
+            'custom' => 'option'
+        ));
+        $option = $element->getOption('custom');
+        $this->assertEquals('option', $option);
+    }
+}
