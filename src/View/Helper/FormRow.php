793c5f7c9677811451531b0ef07d6b8cb4ec1835
terpfear <terpfear@users.noreply.github.com>
2015-10-22 12:02:00 -0400

    Stop FormRow from translating the label twice
    Prevents the label from being translated and processed multiple times. When $labelHelper is invoked with only one argument, it will translate and process the label, and not add anything extra. There is no need for this extra work in FormRow, which has already done this translation. This small change also allows for a more uniform code path when the element has an id and when it does not.

--- src/View/Helper/FormRow.php
+++ src/View/Helper/FormRow.php
@@ -196,14 +196,12 @@ class FormRow extends AbstractHelper
                 // If element has label option `always_wrap` it will be nested in any case.
                 if ($element->hasAttribute('id')
                     && ($element instanceof LabelAwareInterface && !$element->getLabelOption('always_wrap'))
-                ) {
-                    $labelOpen = '';
-                    $labelClose = '';
-                    $label = $labelHelper($element);
+                ) { 
+                    $labelOpen = $labelHelper->openTag($element);
                 } else {
                     $labelOpen  = $labelHelper->openTag($labelAttributes);
-                    $labelClose = $labelHelper->closeTag();
                 }
+                $labelClose = $labelHelper->closeTag();
 
                 if ($label !== '' && (!$element->hasAttribute('id'))
                     || ($element instanceof LabelAwareInterface && $element->getLabelOption('always_wrap'))
