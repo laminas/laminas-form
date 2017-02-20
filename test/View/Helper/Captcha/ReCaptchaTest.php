ceed5140e57ee28ce236b35a45c879790485c99c
Rob Allen <rob@akrabat.com>
2017-02-20 15:39:24 +0000

    Update tests for Recaptcha
    Recaptcha API v2 generates much simpler HTML than API v1 did. Hence we
    can simplify the tests.

--- test/View/Helper/Captcha/ReCaptchaTest.php
+++ test/View/Helper/Captcha/ReCaptchaTest.php
@@ -31,9 +31,8 @@ class ReCaptchaTest extends CommonTestCase
 
         $this->helper  = new ReCaptchaHelper();
         $this->captcha = new ReCaptcha();
-        $service = $this->captcha->getService();
-        $service->setPublicKey(getenv('TESTS_ZEND_FORM_RECAPTCHA_PUBLIC_KEY'));
-        $service->setPrivateKey(getenv('TESTS_ZEND_FORM_RECAPTCHA_PRIVATE_KEY'));
+        $this->captcha->setPubKey(getenv('TESTS_ZEND_FORM_RECAPTCHA_PUBLIC_KEY'));
+        $this->captcha->setPrivKey(getenv('TESTS_ZEND_FORM_RECAPTCHA_PRIVATE_KEY'));
         parent::setUp();
     }
 
@@ -52,62 +51,10 @@ class ReCaptchaTest extends CommonTestCase
         $this->helper->render($element);
     }
 
-    public function testRendersHiddenInputForChallengeField()
-    {
-        $element = $this->getElement();
-        $markup  = $this->helper->render($element);
-        $this->assertRegExp(
-            '#(type="hidden").*?(name="' . $element->getName() . '\&\#x5B;recaptcha_challenge_field\&\#x5D;")#',
-            $markup
-        );
-        $this->assertRegExp('#(type="hidden").*?(id="' . $element->getName() . '-challenge")#', $markup);
-    }
-
-    public function testRendersNoscriptTextareaForChallengeField()
-    {
-        $element = $this->getElement();
-        $markup  = $this->helper->render($element);
-        $this->assertRegExp(
-            '#textarea.*?(name="' . $element->getName() . '\[recaptcha_challenge_field\]")#',
-            $markup
-        );
-    }
-
-    public function testRendersHiddenInputForResponseField()
-    {
-        $element = $this->getElement();
-        $markup  = $this->helper->render($element);
-        $this->assertRegExp(
-            '#(type="hidden")[^>]*?(name="' . $element->getName() . '\&\#x5B;recaptcha_response_field\&\#x5D;")'
-            . '[^>]*?(id="' . $element->getName() . '-response")#',
-            $markup
-        );
-    }
-
-    public function testRendersNoscriptHiddenInputForResponseField()
-    {
-        $element = $this->getElement();
-        $markup  = $this->helper->render($element);
-        $this->assertRegExp(
-            '#(type="hidden")[^>]*?(name="' . $element->getName() . '\[recaptcha_response_field\]")'
-            . '[^>]*?(value="manual_challenge")#',
-            $markup
-        );
-    }
-
     public function testRendersReCaptchaMarkup()
     {
         $element = $this->getElement();
         $markup  = $this->helper->render($element);
         $this->assertContains($this->captcha->getService()->getHtml($element->getName()), $markup);
-    }
-
-    public function testRendersJsEventScripts()
-    {
-        $element = $this->getElement();
-        $markup  = $this->helper->render($element);
-        $this->assertContains('function zendBindEvent', $markup);
-        $this->assertContains('document.getElementById("' . $element->getName() . '-challenge")', $markup);
-        $this->assertContains('document.getElementById("' . $element->getName() . '-response")', $markup);
     }
 }
