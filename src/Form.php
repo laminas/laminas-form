26fcee5a6a2722e7a12e0224874ed1332fb582ad
Robin Keet <robin@keet.me>
2018-07-22 20:29:56 +0200

    Updated Form class, bindValues function to set  to empty array if no data to pass along for baseFieldset

--- src/Form.php
+++ src/Form.php
@@ -361,7 +361,7 @@ class Form extends Fieldset implements FormInterface
 
         // If there is a base fieldset, only hydrate beginning from the base fieldset
         if ($this->baseFieldset !== null) {
-            $data = $data[$this->baseFieldset->getName()];
+            $data = isset($data[$this->baseFieldset->getName()]) ? $data[$this->baseFieldset->getName()] : [];
             $this->object = $this->baseFieldset->bindValues($data, $validationGroup[$this->baseFieldset->getName()]);
         } else {
             $this->object = parent::bindValues($data, $validationGroup);
