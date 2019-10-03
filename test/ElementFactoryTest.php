cd26c63bd9b33fc66b42da2facb516dab38163d4
webimpress <contact@webimpress.com>
2019-10-03 19:41:51 +0100

    Fixed tests

--- test/ElementFactoryTest.php
+++ test/ElementFactoryTest.php
@@ -1,40 +1,64 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2019 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-form for the canonical source repository
+ * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-form/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\Form;
 
 use ArrayObject;
+use Interop\Container\ContainerInterface;
 use PHPUnit\Framework\TestCase;
 use Zend\Form\ElementFactory;
+use Zend\ServiceManager\Exception\InvalidServiceException;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use ZendTest\Form\TestAsset\ArgumentRecorder;
 
-/**
- * @group      Zend_Form
- */
-class FormElementManagerTest extends TestCase
+class ElementFactoryTest extends TestCase
 {
-    public function testCreationOptionsHandled()
+    public function validCreationOptions()
     {
-        $this->assertCreationOptionsOfServiceAreSet(['key' => 'value'], new ArrayObject(['key' => 'value']));
-        $this->assertCreationOptionsOfServiceAreSet(['key' => 'value'], ['key' => 'value']);
-        $this->assertCreationOptionsOfServiceAreSet([], []);
-        $this->assertCreationOptionsOfServiceAreSet([], null);
+        yield 'ArrayObject' => [new ArrayObject(['key' => 'value']), ['key' => 'value']];
+        yield 'array' => [['key' => 'value'], ['key' => 'value']];
+        yield 'empty-array' => [[], []];
+        yield 'null' => [null, []];
     }
 
-    private function assertCreationOptionsOfServiceAreSet($expectedValue, $input)
+    /**
+     * @dataProvider validCreationOptions
+     *
+     * @param mixed $creationOptions
+     * @param array $expectedValue
+     */
+    public function testValidCreationOptions($creationOptions, array $expectedValue)
     {
-        $serviceLocator = $this->prophesize(ServiceLocatorInterface::class)->reveal();
+        $container = $this->prophesize(ServiceLocatorInterface::class)
+            ->willImplement(ContainerInterface::class)
+            ->reveal();
 
-        $factory = new ElementFactory($input);
-        $result = $factory->createService($serviceLocator, ArgumentRecorder::class);
+        $factory = new ElementFactory($creationOptions);
+        $result = $factory->createService($container, ArgumentRecorder::class);
         $this->assertInstanceOf(ArgumentRecorder::class, $result);
         $this->assertSame(['argumentrecorder', $expectedValue], $result->args);
+    }
+
+    public function invalidCreationOptions()
+    {
+        yield 'object' => [(object) ['key' => 'val']];
+        yield 'int' => [PHP_INT_MAX];
+        yield 'string' => [uniqid('', true)];
+    }
+
+    /**
+     * @dataProvider invalidCreationOptions
+     *
+     * @param $creationOptions
+     */
+    public function testInvalidCreationOptionsException($creationOptions)
+    {
+        $this->expectException(InvalidServiceException::class);
+        $this->expectExceptionMessage('cannot use non-array, non-traversable, non-null creation options;');
+        new ElementFactory($creationOptions);
     }
 }
--- test/TestAsset/ArgumentRecorder.php
+++ test/TestAsset/ArgumentRecorder.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2019 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/zendframework/zend-form for the canonical source repository
+ * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-form/blob/master/LICENSE.md New BSD License
  */
 
 namespace ZendTest\Form\TestAsset;
 
-class ArgumentRecorder {
+class ArgumentRecorder
+{
     public $args;
-    public function __construct(...$args) {
+
+    public function __construct(...$args)
+    {
         $this->args = $args;
     }
 }
