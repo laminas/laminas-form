35c4496aafaa510dd146375e0bf11fa50582f201
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:57:04 -0600

    Rewriting as Laminas Project package

--- src/Annotation/AllowEmpty.php
+++ src/Annotation/AllowEmpty.php
@@ -1,21 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-form for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-form/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-form/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Form\Annotation;
+namespace Laminas\Form\Annotation;
 
-use Zend\Filter\Boolean as BooleanFilter;
+use Laminas\Filter\Boolean as BooleanFilter;
 
 /**
  * AllowEmpty annotation
  *
  * Presence of this annotation is a hint that the associated
- * \Zend\InputFilter\Input should enable the allowEmpty flag.
+ * \Laminas\InputFilter\Input should enable the allowEmpty flag.
  *
  * @Annotation
  */
--- src/Element/Password.php
+++ src/Element/Password.php
@@ -1,17 +1,16 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright  Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @see       https://github.com/laminas/laminas-form for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-form/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-form/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Form\Element;
+namespace Laminas\Form\Element;
 
-use Zend\Form\Element;
-use Zend\Form\ElementPrepareAwareInterface;
-use Zend\Form\FormInterface;
+use Laminas\Form\Element;
+use Laminas\Form\ElementPrepareAwareInterface;
+use Laminas\Form\FormInterface;
 
 class Password extends Element implements ElementPrepareAwareInterface
 {
