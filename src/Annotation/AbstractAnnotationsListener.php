29e1a38d42503143b43337b271d46ff35b1361dd
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:56:43 -0600

    Rewriting as Laminas Project package

--- src/Annotation/AbstractAnnotationsListener.php
+++ src/Annotation/AbstractAnnotationsListener.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-form for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-form/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-form/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Form\Annotation;
+namespace Laminas\Form\Annotation;
 
+use Laminas\EventManager\AbstractListenerAggregate;
+use Laminas\EventManager\EventManagerInterface;
+use Laminas\EventManager\ListenerAggregateInterface;
 use ReflectionClass;
-use Zend\EventManager\AbstractListenerAggregate;
-use Zend\EventManager\EventManagerInterface;
-use Zend\EventManager\ListenerAggregateInterface;
 
 /**
  * Base annotations listener.
@@ -29,14 +28,14 @@ abstract class AbstractAnnotationsListener extends AbstractListenerAggregate
     /**
      * Attempt to discover a name set via annotation
      *
-     * @param  \Zend\EventManager\EventInterface $e
+     * @param  \Laminas\EventManager\EventInterface $e
      * @return false|string
      */
     public function handleNameAnnotation($e)
     {
         $annotations = $e->getParam('annotations');
 
-        if (!$annotations->hasAnnotation('Zend\Form\Annotation\Name')) {
+        if (!$annotations->hasAnnotation('Laminas\Form\Annotation\Name')) {
             return false;
         }
 
@@ -53,7 +52,7 @@ abstract class AbstractAnnotationsListener extends AbstractListenerAggregate
     /**
      * Discover the fallback name via reflection
      *
-     * @param  \Zend\EventManager\EventInterface $e
+     * @param  \Laminas\EventManager\EventInterface $e
      * @return string
      */
     public function discoverFallbackName($e)
