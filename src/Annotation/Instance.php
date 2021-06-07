41ecde4a9871798f7e7856b34ebcaffba6f3c98d
Filippo Tessarotto <zoeslam@gmail.com>
2021-06-07 10:03:11 +0200

    Remove deprecated Instance::getObject
    Signed-off-by: Filippo Tessarotto <zoeslam@gmail.com>

--- src/Annotation/Instance.php
+++ src/Annotation/Instance.php
@@ -43,20 +43,4 @@ final class Instance
     {
         return $this->instance;
     }
-
-    /**
-     * Retrieve the instance
-     *
-     * @deprecated 3.0.0 Use getInstance() instead
-     */
-    public function getObject(): string
-    {
-        trigger_error(sprintf(
-            'Calling %s::%s is deprecated since 3.0.0, use getInstance() instead.',
-            static::class,
-            __METHOD__
-        ), E_USER_DEPRECATED);
-
-        return $this->getInstance();
-    }
 }
