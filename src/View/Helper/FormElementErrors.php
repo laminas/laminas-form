72261b20fc36bf4cd695c872760245c21156fc7d
Filippo Tessarotto <zoeslam@gmail.com>
2022-02-01 08:30:09 +0100

    Backport GHSA-jq4p-mq33-w375 to v3.0
    Signed-off-by: Filippo Tessarotto <zoeslam@gmail.com>

--- src/View/Helper/FormElementErrors.php
+++ src/View/Helper/FormElementErrors.php
@@ -9,6 +9,7 @@ use Laminas\Form\Exception;
 
 use function array_merge;
 use function array_walk_recursive;
+use function count;
 use function implode;
 use function sprintf;
 
@@ -75,6 +76,12 @@ class FormElementErrors extends AbstractHelper
         $attributes = $this->createAttributesString($attributes);
         if (! empty($attributes)) {
             $attributes = ' ' . $attributes;
+        }
+
+        $count   = count($messages);
+        $escaper = $this->getEscapeHtmlHelper();
+        for ($i = 0; $i < $count; $i += 1) {
+            $messages[$i] = $escaper($messages[$i]);
         }
 
         // Generate markup
