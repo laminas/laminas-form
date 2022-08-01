faefb5bbe578a071b95f3a34a145940a28b2acee
Marco Pivetta <ocramius@gmail.com>
2022-08-01 11:39:38 +0200

    Tests for #160 and #184: showing broken datetime picker rendering in `es_CL`+short formats
    This change introduces verbose XML-alike-structure verification, which checks:
    
     1. that the HTML generated by `FormDateSelect` and `FormDateTimeSelect`
        matches the requested locale (we had zero tests verifying the generated HTML structure!)
     2. that the HTML generation actually works (fails on `3.4.x` / 0ca41651cee8486cef68b6e0281ef9be99379c78 )
    
    The underlying issue was that the date format was being parsed by a bogus regular expression (fixed
    in previous commit), which in turn led to a PHP warning being raised, as well as HTML rendering being
    completely broken.
    
    This test validates the work done by @mimmi20 in #184, and also highlights how antiquate
    these HTML select elements are (please use `<input type="datetime-local"/>` or similar instead).
    
    Signed-off-by: Marco Pivetta <ocramius@gmail.com>

--- test/View/Helper/FormDateSelectTest.php
+++ test/View/Helper/FormDateSelectTest.php
@@ -9,8 +9,6 @@ use Laminas\Form\Element\DateSelect;
 use Laminas\Form\Element\Select;
 use Laminas\Form\Exception\DomainException;
 use Laminas\Form\View\Helper\FormDateSelect as FormDateSelectHelper;
-use ReflectionException;
-use ReflectionMethod;
 
 use function extension_loaded;
 
@@ -119,26 +117,82 @@ final class FormDateSelectTest extends AbstractCommonTestCase
     }
 
     /**
-     * @group issue-160
-     * @throws ReflectionException
+     * The `es_CL` locale has a short date pattern of `dd-MM-yy HH:mm:ss`, which should be
+     * supported by our internal pattern parsing logic.
+     *
+     * This test verifies that such a pattern is correctly identified internally, and that
+     * our date rendering matches the pattern too.
+     *
+     * This kind of test could be repeated for all defined locales, but `es_CL` is sufficient
+     * for now, since this test was initially designed to catch a regression in the logic
+     * that reads out `ext-intl` patterns for us.
+     *
+     * @group 160
+     * @group 184
      */
-    public function testParsePatternWithShortDateFormat(): void
+    public function testRendersDatesWithEsCLLocaleDatePattern(): void
     {
-        $element  = new DateSelect('foo');
-        $dateType = IntlDateFormatter::SHORT;
-        $locale   = 'es_CL';
+        $this->helper->setLocale('es_CL');
+        $this->helper->setDateType(IntlDateFormatter::SHORT);
 
-        $this->helper->setLocale($locale);
-        $this->helper->setDateType($dateType);
+        $element = new DateSelect('foo');
+        $element->setMinYear(2022);
+        $element->setMaxYear(2022);
 
-        $method = new ReflectionMethod($this->helper, 'parsePattern');
-        $method->setAccessible(true);
-
-        $pattern = $method->invoke($this->helper, $element->shouldRenderDelimiters());
-
-        $this->assertIsArray($pattern);
-        $this->assertArrayHasKey('day', $pattern);
-        $this->assertArrayHasKey('month', $pattern);
-        $this->assertArrayHasKey('year', $pattern);
+        self::assertXmlStringEqualsXmlString(
+            <<<'XML'
+<html>
+    <select name="day">
+        <option value="01">01</option>
+        <option value="02">02</option>
+        <option value="03">03</option>
+        <option value="04">04</option>
+        <option value="05">05</option>
+        <option value="06">06</option>
+        <option value="07">07</option>
+        <option value="08">08</option>
+        <option value="09">09</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
+        <option value="20">20</option>
+        <option value="21">21</option>
+        <option value="22">22</option>
+        <option value="23">23</option>
+        <option value="24">24</option>
+        <option value="25">25</option>
+        <option value="26">26</option>
+        <option value="27">27</option>
+        <option value="28">28</option>
+        <option value="29">29</option>
+        <option value="30">30</option>
+        <option value="31">31</option>
+    </select>-<select name="month">
+        <option value="01">01</option>
+        <option value="02">02</option>
+        <option value="03">03</option>
+        <option value="04">04</option>
+        <option value="05">05</option>
+        <option value="06">06</option>
+        <option value="07">07</option>
+        <option value="08">08</option>
+        <option value="09">09</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+    </select>-<select name="year">
+        <option value="2022">2022</option>
+    </select>
+</html>
+XML,
+            '<html>' . $this->helper->render($element) . '</html>'
+        );
     }
 }
