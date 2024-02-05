49bd34c6749664e98dfea492f4459b0723a94929
Paul McGarry <paul@paulmcgarry.com>
2024-02-06 10:34:18 +1100

    Additional consistency with other helper getters.
    Signed-off-by: Paul McGarry <paul@paulmcgarry.com>

--- src/View/Helper/FormSelect.php
+++ src/View/Helper/FormSelect.php
@@ -297,14 +297,16 @@ class FormSelect extends AbstractHelper
 
     protected function getFormHiddenHelper(): FormHidden
     {
-        if (! $this->formHiddenHelper) {
-            if ($this->view && method_exists($this->view, 'plugin')) {
-                $this->formHiddenHelper = $this->view->plugin('formhidden');
-            }
+        if (null !== $this->formHiddenHelper) {
+            return $this->formHiddenHelper;
+        }
 
-            if (! $this->formHiddenHelper instanceof FormHidden) {
-                $this->formHiddenHelper = new FormHidden();
-            }
+        if ($this->view && method_exists($this->view, 'plugin')) {
+            $this->formHiddenHelper = $this->view->plugin('formhidden');
+        }
+
+        if (! $this->formHiddenHelper instanceof FormHidden) {
+            $this->formHiddenHelper = new FormHidden();
         }
 
         return $this->formHiddenHelper;
